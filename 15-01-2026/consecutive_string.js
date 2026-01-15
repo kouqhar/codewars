2624d8784ac20b986aa4c40e04643ce8223148f3
kouqhar <naphtaliduniya2@gmail.com>
2026-01-15 15:24:33 +0000

    15-01-2026

---
+++ 15-01-2026/consecutive_string.js
@@ -0,0 +1,15 @@
+function longestConsec(strarr, k) {
+  let finalString = "";
+  if (!strarr || k <= 0 || k > strarr.length) return finalString;
+  else {
+    strarr.forEach((item, index) => {
+      if (index === strarr.length) return;
+      else {
+        const concatenated = strarr.slice(index, index + k).join("");
+        const concatenatedLength = concatenated.length;
+        if (concatenatedLength > finalString.length) finalString = concatenated;
+      }
+    });
+  }
+  return finalString;
+}
