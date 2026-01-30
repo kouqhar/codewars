168384abb6ea6f0ec5a39b7e2de9d5c6996d6243
kouqhar <naphtaliduniya2@gmail.com>
2026-01-30 23:51:21 +0000

    Fibonacci, Tribonacci and friends

---
+++ 31-01-2026/fibonacci_tribonacci_friends.js
@@ -0,0 +1,20 @@
+const Xbonacci = (signature, n) => {
+  let orgArr = [...signature];
+  let [len, sigLen, arr] = [orgArr.length, signature.length, []];
+
+  if (n === 0) return [];
+  else if (len > n) orgArr = signature.slice(0, n);
+  else {
+    for (let i = len; i < n; i++) {
+      if (i <= n) {
+        arr = orgArr.slice(-sigLen);
+        let nextValue = arr.reduce((acc, curr) => acc + curr, 0);
+        orgArr.push(nextValue);
+        arr = [];
+      } else {
+        return 1;
+      }
+    }
+  }
+  return orgArr;
+};
