6764e0ebb64fab7f86c037677d2d5aa9eb969368
kouqhar <naphtaliduniya2@gmail.com>
2026-01-31 00:13:20 +0000

    Write Number in Expanded Form

--- 31-01-2026/number_expanded_form.js
+++ 31-01-2026/number_expanded_form.js
@@ -2,13 +2,9 @@ const expandedForm = (arr) => {
   arr = arr.toString().split("");
   let [pow, decimal, len] = [[], [], arr.length];
 
-  for (let i = 0; i <= len - 1; i++) {
-    pow.unshift(i);
-  }
-  arr.forEach((x, index) => {
-    x = parseInt(x);
-    decimal.push(x * 10 ** pow[index]);
-  });
+  for (let i = 0; i <= len - 1; i++) pow.unshift(i);
+
+  arr.forEach((x, index) => decimal.push(parseInt(x) * 10 ** pow[index]));
 
   return decimal
     .filter((a) => a !== 0)
