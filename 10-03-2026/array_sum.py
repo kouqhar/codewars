0a264bcebfbc5b3b75e361ca88173b10bfbf8e3a
kouqhar <naphtaliduniya2@gmail.com>
2026-03-10 13:41:58 +0000

    Array sum py

---
+++ 10-03-2026/array_sum.py
@@ -0,0 +1,9 @@
+def sum_array(numbers):
+
+    return sum(numbers)
+
+print(sum_array([1, 2, 3, 4, 5])); # 15.
+print(sum_array([42])); # 42.
+print(sum_array([5, -2, 7, -3])); # 7.
+print(sum_array([203, 145, -129, 6293, 523, -919, 845, 2434])); # 9395.
+print(sum_array([0, 0])); # 0.
