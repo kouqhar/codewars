16d3ce34875ca1c826c73e130c4d6cc709d54b46
kouqhar <naphtaliduniya2@gmail.com>
2026-03-28 17:39:07 +0000

    Movie Night py

--- 28-03-2026/README.md
+++ 28-03-2026/README.md
@@ -1,3 +1,29 @@
 ## Passing Exam Count
 
 Given an array of student exam scores and the score needed to pass it, return the number of students that passed the exam.
+
+## Movie Night
+
+Given a string for the day of the week, another string for a showtime, and an integer number of tickets, return the total cost of the movie tickets for that showing.
+
+The given day will be one of:
+
+"Monday"
+"Tuesday"
+"Wednesday"
+"Thursday"
+"Friday"
+"Saturday"
+"Sunday"
+
+The showtime will be given in the format "H:MMam" or "H:MMpm". For example "10:00am" or "10:00pm".
+
+Return the total cost in the format "$D.CC" using these rules:
+
+Weekend (Friday - Sunday): $12.00 per ticket.
+
+Weekday (Monday - Thursday): $10.00 per ticket.
+
+Matinee (before 5:00pm): subtract $2.00 per ticket (except on Tuesdays).
+
+Tuesdays: all tickets are $5.00 each.
---
+++ 28-03-2026/movie_night.py
@@ -0,0 +1,24 @@
+def get_movie_night_cost(day, showtime, number_of_tickets):
+    weekend = ["Friday", "Saturday", "Sunday"]
+    weekday = ["Monday", "Wednesday", "Thursday"]
+    [[hour, minute], period, cost] = [showtime[:-2].split(":"), showtime[-2:], 0]
+
+    if day in weekend:
+        cost = 12.00
+    elif day in weekday:
+        cost = 10.00
+    else:
+        return f"${5.00 * number_of_tickets:.2f}"
+
+    total = cost * number_of_tickets
+    matinee = total - (2.00 * number_of_tickets) if (period == "pm" and int(hour)) < 5 else total
+
+    return f"${matinee:.2f}"
+
+print(get_movie_night_cost("Saturday", "10:00pm", 1)) # "$12.00".
+print(get_movie_night_cost("Sunday", "10:00am", 1)) # "$10.00".
+print(get_movie_night_cost("Tuesday", "7:20pm", 2)) # "$10.00".
+print(get_movie_night_cost("Wednesday", "5:40pm", 3)) # "$30.00".
+print(get_movie_night_cost("Monday", "11:50am", 4)) # "$32.00".
+print(get_movie_night_cost("Friday", "4:30pm", 5)) # "$50.00".
+print(get_movie_night_cost("Tuesday", "11:30am", 1)) # "$5.00".
