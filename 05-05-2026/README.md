3a97fe05ecd08ddbfb18909ce8b68027b42d7312
kouqhar <naphtaliduniya2@gmail.com>
2026-05-05 20:54:35 +0100

    Narcissistic Number js

---
+++ 05-05-2026/README.md
@@ -0,0 +1,7 @@
+## Narcissistic Number
+
+Given a positive integer, determine whether it is a narcissistic number.
+
+A number is narcissistic if the sum of each of its digits raised to the power of the total number of digits equals the number itself.
+
+For example, 153 has 3 digits, and 13 + 53 + 33 = 153, so it is narcissistic.
---
+++ 05-05-2026/narcissistic_number.js
@@ -0,0 +1,20 @@
+function isNarcissistic(n) {
+  return (
+    n
+      .toString()
+      .split("")
+      .reduce(
+        (acc, digit) => acc + Math.pow(parseInt(digit), n.toString().length),
+        0,
+      ) === parseInt(n)
+  );
+}
+
+console.log(isNarcissistic(153)); // true
+// console.log(isNarcissistic(154)); // false
+// console.log(isNarcissistic(371)); // true
+// console.log(isNarcissistic(512)); // false
+// console.log(isNarcissistic(9)); // true
+// console.log(isNarcissistic(11)); // false
+// console.log(isNarcissistic(9474)); // true
+// console.log(isNarcissistic(6549)); // false
