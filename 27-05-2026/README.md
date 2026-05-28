a219e93aae9fc89b3eaae3184beb29c4a239b3f5
kouqhar <naphtaliduniya2@gmail.com>
2026-05-28 17:17:34 +0100

    FizzBuzz Count py

--- 27-05-2026/README.md
+++ 27-05-2026/README.md
@@ -9,3 +9,15 @@ You can't eat a partial slice, so round each person's slice count up to the near
 Each person gets a minimum of two slices.
 
 Each pizza has 8 slices. Round the total number of pizzas up to the nearest whole pizza.
+
+## FizzBuzz Count
+
+Given a start and end number, count the number of fizz and buzz appearances in the range (inclusive).
+
+Numbers divisible by 3 count as a fizz.
+
+Numbers divisible by 5 count as a buzz.
+
+Numbers divisible by both 3 and 5 count as both a fizz and a buzz.
+
+Return an object or dictionary with the counts in the format: { fizz, buzz }.
---
+++ 27-05-2026/fizzbuzz_count.py
@@ -0,0 +1,24 @@
+def fizz_buzz_count(start, end):
+    numArray = {
+        "fizz": 0,
+        "buzz": 0,
+    }
+
+    for i in range(start, end + 1):
+        elementIsFizz, elementIsBuzz = i % 3 == 0, i % 5 == 0
+
+        if elementIsFizz and elementIsBuzz:
+            numArray["fizz"] += 1
+            numArray["buzz"] += 1
+        elif elementIsFizz:
+            numArray["fizz"] += 1
+        elif elementIsBuzz:
+            numArray["buzz"] += 1
+
+    return numArray
+
+print(fizz_buzz_count(1, 11)) # {"fizz": 3, "buzz": 2}.
+print(fizz_buzz_count(14, 41)) # {"fizz": 9, "buzz": 6}.
+print(fizz_buzz_count(24, 100)) # {"fizz": 26, "buzz": 16}.
+print(fizz_buzz_count(-635, -14)) # {"fizz": 207, "buzz": 125}.
+print(fizz_buzz_count(-5432, 6789)) # {"fizz": 4074, "buzz": 2444}.
